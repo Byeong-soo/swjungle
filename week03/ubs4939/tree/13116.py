d949929d5a17995223d7a35719067edd687c38c8
ubs <ubs4939@naver.com>
2022-10-07 20:12:56 +0900

    백준 13116 (답지) - 트리 순회 기본 문제 - 코드 차이가 없는데 내꺼 통과를 못함... 원인 찾아볼것 ( 해결) - mutable immutable 일떄 데이터 변경 잘 고민할것

--- week03/ubs4939/tree/13116.py
+++ week03/ubs4939/tree/13116.py
@@ -4,64 +4,64 @@ import sys
 def check_root(number):
     result = []
 
-    if number <= 1:
+    if number <= 0:
         return result
-    number //= 2
     result.append(number)
+    number //= 2
 
     return result + check_root(number)
 
 
-def check_root2(tree, number):
+def check_root2(number):
     if number <= 0:
         return
 
     if not tree[number]:
         tree[number] = True
-        check_root2(tree, number // 2)
+        check_root2(number // 2)
 
 
-def check_root3(tree, number):
+def check_root3(number):
     if number <= 0:
         return
     if tree[number]:
         print(number * 10)
         return
     else:
-        check_root3(tree, number // 2)
+        check_root3(number // 2)
 
 
 if __name__ == '__main__':
     test_count = int(sys.stdin.readline().rstrip())
-    tree = [False for x in range(0, 1024)]
 
     for _ in range(test_count):
+        tree = [False for x in range(0, 1024)]
         f_num, s_num = map(int, sys.stdin.readline().rstrip().split(" "))
-
-        while True:
-            if f_num == s_num:
-                print(f_num * 10)
-                break
-
-            if f_num > s_num:
-                f_num //= 2
-            else:
-                s_num //= 2
-
-        # if a < b:
-        #     temp = a
-        #     a = b
-        #     b = temp
         #
-        # check_root2(tree,a)
-        # check_root3(tree,b)
+        # while True:
+        #     if f_num == s_num:
+        #         print(f_num * 10)
+        #         break
+        #
+        #     if f_num > s_num:
+        #         f_num //= 2
+        #     else:
+        #         s_num //= 2
+
+        if f_num < s_num:
+            temp = f_num
+            f_num = s_num
+            s_num =temp
+
+        check_root2(f_num)
+        check_root3(s_num)
 
         # result1 = check_root(f_num)
         # result2 = check_root(s_num)
-        #
+
         # print(result1)
         # print(result2)
-        #
+
         # check = False
         #
         # for num2 in result2:
